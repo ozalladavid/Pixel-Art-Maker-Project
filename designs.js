8c0ebd913f361f00ce19776d97de1f3b50c15fe7
ozalla david chekwube <33552218+ozalladavid@users.noreply.github.com>
2018-06-27 14:24:02 +0100

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,8 +3,35 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
-// Your code goes here!
-
+function makeGrid()  {
+  let w, h;
+  w = $('#input_width').val();
+  h = $('#input_height').val();
+  for(tr=1; tr<=h; tr++) { // Build table rows
+    $('#pixel_canvas').append('<tr></tr>');
+  }
+  for(td=1; td<=w; td++){ // Build table columns
+    $('tr').append('<td></td>');
+  }
 }
+
+// Submit button
+$('#sizePicker').submit(function(e) {
+  e.preventDefault();  //prevent form from submitting
+  let tr = $('#pixel_canvas').find('tr');
+  tr.remove();
+  makeGrid();
+});
+
+// Aplying color to td's background
+$('#pixel_canvas').on('click','td', function(e) {
+  let color = $('#colorPicker').val();
+  $(e.target).css('background-color', color);
+});
+
+// Test alert
+// $('#sizePicker').submit(function(e) {
+//     e.preventDefault();  //prevent form from submitting
+//     alert('Handler for .submit() called.' + makeGrid());
+// });
+
